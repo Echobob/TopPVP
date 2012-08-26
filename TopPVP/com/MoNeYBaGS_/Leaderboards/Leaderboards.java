777c71c052edb978aba2f1dfecf791d28e47b433
Alan Campbell <echobob@theminequest.com>
2012-08-26 15:16:50 -0400

    Revert "Refactor and Converted to maven project."
    This reverts commit 819271cbe08fcd1eb8e8a8cc7b4a25c7e31bf57c.

---
+++ TopPVP/com/MoNeYBaGS_/Leaderboards/Leaderboards.java
@@ -0,0 +1,33 @@
+package com.MoNeYBaGS_.Leaderboards;
+
+import java.util.Map;
+
+import com.MoNeYBaGS_.TopPVP;
+
+
+public class Leaderboards {
+	
+	private final TopPVP plugin;
+	private Players lead = null;
+	
+	public Leaderboards(TopPVP instance)
+	{
+		this.plugin = instance;
+		lead = new Players(plugin);
+	}
+	
+	public Map<String, Integer> getKillsLeaderboards()
+	{
+		return lead.getKillsLeaderboard();
+	}
+	
+	public Map<String, Double> getKDRLeaderboards()
+	{
+		return lead.getKDRLeaderboards();
+	}
+	
+	public Map<String, Integer> getDeathsLeaderboards()
+	{
+		return lead.getDeathsLeaderboards();
+	}
+}
